d8ad27a887608d90747f40f578e7567d403f244e
A-phenomenal1 <niteshch200053@gmail.com>
2023-02-10 13:49:21 +0530

    user authentication

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { deleteCookie } from "../../utils/cookie";
 
-export default function HomePage() {
-    return (
-        <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
-            <Link to="/">
-                <button className="primary-button">Log out</button>
-            </Link>
-        </div>
-    )
+export default function HomePage(props) {
+  const handleLogout = () => {
+    deleteCookie("auth");
+    window.location.href = "/";
+  };
+  return (
+    <div className="text-center">
+      <h1 className="main-title home-page-title">welcome to our app {props.name}</h1>
+
+      <button className="primary-button" onClick={handleLogout}>
+        Log out
+      </button>
+    </div>
+  );
 }
